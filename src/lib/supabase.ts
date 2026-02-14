33dde68963dff2904b124f77aca0540d82459d99
Schoollog <affiliateshopTP@web.de>
2026-02-14 13:07:55 +0100

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,8 @@
+// src/app/lib/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
-// Diese Werte findest du in deinem Supabase Dashboard
-const supabaseUrl = process.env.https://lnxngycslbmbspufbdmh.supabase.co || '';
-const supabaseAnonKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxueG5neWNzbGJtYnNwdWZiZG1oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEwNTUzNzAsImV4cCI6MjA4NjYzMTM3MH0.ryMcI-7jzbbfkxBQbQBSpdVlMeLpX27V2_9pbv3lSR8 || '';
+// Ersetze diese Platzhalter mit deinen echten Daten von Supabase
+const supabaseUrl = 'https://lnxngycslbmbspufbdmh.supabase.co'; 
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxueG5neWNzbGJtYnNwdWZiZG1oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEwNTUzNzAsImV4cCI6MjA4NjYzMTM3MH0.ryMcI-7jzbbfkxBQbQBSpdVlMeLpX27V2_9pbv3lSR8';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
